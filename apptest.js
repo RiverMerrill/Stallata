aa9fdd14d507b590f56ec13f27fc2103ac4afcd8
River Merrill <rivermerrill.me@gmail.com>
2018-05-18 14:49:37 +0000

    ui updates

--- apptest.js
+++ apptest.js
@@ -1,8 +1,17 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').Server(app);
 
+db.defaults({test: []}).write();
+
+db.get('test')
+	.push({id: 1})
+	.write();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
